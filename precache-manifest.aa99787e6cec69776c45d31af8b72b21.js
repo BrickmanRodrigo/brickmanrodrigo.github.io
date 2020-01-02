c648f2c777dcc7c2b8825133ace51f19512b0cf4
Rodrigo Brickman <brickmanrodrigo@gmail.com>
2020-01-02 20:27:12 -0300

    Updates

---
+++ precache-manifest.aa99787e6cec69776c45d31af8b72b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7956758fb0f2c4b8dc5c93842037c84b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1d3c31b3fc078522c65",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3d3291768ea17e73a253",
+    "url": "/static/js/2.feff552f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.feff552f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1d3c31b3fc078522c65",
+    "url": "/static/js/main.a013cdc5.chunk.js"
+  },
+  {
+    "revision": "4420b44f21d1f0e00bb4",
+    "url": "/static/js/runtime-main.a8f39ccc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
